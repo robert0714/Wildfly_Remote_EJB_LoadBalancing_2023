79479b915dd7e66b3b3523aaa47e9b428699d89d
Robert <robert0714@gmail.com>
2023-06-09 08:52:38 +0800

    fixbugs

--- cluster-example-client/wildfly/main/java/de/akquinet/jbosscc/cluster/client/rs/TestController.java
+++ cluster-example-client/wildfly/main/java/de/akquinet/jbosscc/cluster/client/rs/TestController.java
@@ -44,8 +44,8 @@ public class TestController {
 		try {
 			RemoteStateless statelessProxy = remoteEJBClient.lookupRemoteStatelessBean();
 			nodeName = statelessProxy.getNodeName();
-            log.info("nodeName: "+ nodeName); 
-		} catch (Exception e) {
+                        log.info("nodeName: "+ nodeName); 
+		 
 		} catch (Exception e) {
 			log.error(e.getMessage(), e); 
 		}
